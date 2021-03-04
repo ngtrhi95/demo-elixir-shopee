6cdf851f0dcfec742aaf923757d9241d2fcf86cb
ngtrhi95 <trhien1395@gmail.com>
2021-03-04 16:28:42 +0700

    updates for heroku

---
+++ config/prod.exs
@@ -0,0 +1,18 @@
+use Mix.Config
+
+config :project_manager_example, Ecommerce.Endpoint,
+  load_from_system_env: true,
+  url: [scheme: "https", host: "polar-eyrie-42502.herokuapp.com/", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+
+# Do not print debug messages in production
+config :logger, level: :info
+
+# Configure your database
+config :project_manager_example, Ecommerce.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
